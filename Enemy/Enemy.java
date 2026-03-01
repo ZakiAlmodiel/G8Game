af3b38e1e8e78896be88aa6dced24f52508cf47b
Shinonaguwa <shizusama2006@gmail.com>
2026-03-01 22:00:46 +0800

    Finished (I think)

--- Enemy/Enemy.java
+++ Enemy/Enemy.java
@@ -1,5 +1,3 @@
-package Enemy;
-
 public class Enemy{
     public String enemyName;
     public int enemyHp;
@@ -13,6 +11,29 @@ public class Enemy{
         this.enemyHp = hp;
         this.enemyDamage = attack;
     }
-
     
+    
+    public void enemyAttack(Hero hero){  // Attack Computations
+        int damage = 1*enemyDamage;
+        hero.heroHp -= 1*enemyDamage;
+        System.out.println();
+        System.out.print(
+            enemyName+" has attacked you! Dealt "+
+            damage+" damage"
+        );
+    }
+    
+    public void enemyDefeatedDialogue(){    // Sets of dialogues
+        String[] deathDialogues = {
+            "Enemy: Nooo... my gold...",
+            "Enemy: Impossible... I'm... strong...",
+            "Enemy: My spells... failed me...",
+            "Enemy: Death... takes me again...",
+            "Enemy: I... am... eternal..."
+        };
+        System.out.printf("\n%s\n", deathDialogues[dialogueCounter]);
+        dialogueCounter++;
+    }
+
 }
+
