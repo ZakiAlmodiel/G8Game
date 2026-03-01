ed810befb8072f81edac034c8be93303d479914e
Shinonaguwa <shizusama2006@gmail.com>
2026-03-01 18:43:24 +0800

    Initial commit of game classes and story logic

---
+++ G8Game-main/Heroes/Mist.java
@@ -0,0 +1,12 @@
+public void displayBackStory(){
+        
+        System.out.println("-----------------BACKSTORY-------------------");
+        System.out.println();
+        System.out.println(
+        "Mist had been a lone hunter ever since he was a child."+
+         "He wielded claw katars made from antlers,\n"+
+         "ensuring that the animals he hunted stayed dead (close-combat)."+
+         "One night, while searching for food \nin the rainforest,"+
+         "he encountered a strange, flood-like cloud sweeping through the trees."+
+         "He tried \nto run, but its speed was just unfortunately great…..");
+    }
